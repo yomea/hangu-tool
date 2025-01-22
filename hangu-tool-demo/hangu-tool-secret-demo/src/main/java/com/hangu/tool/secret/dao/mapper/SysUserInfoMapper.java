6d866a33d1925eda7a354e23274a4070a74121ae
zhen_hong <951645267@qq.com>
2025-01-22 09:46:04 +0800

    feat：add more test case

--- hangu-tool-demo/hangu-tool-secret-demo/src/main/java/com/hangu/tool/secret/dao/mapper/SysUserInfoMapper.java
+++ hangu-tool-demo/hangu-tool-secret-demo/src/main/java/com/hangu/tool/secret/dao/mapper/SysUserInfoMapper.java
@@ -3,6 +3,7 @@ package com.hangu.tool.secret.dao.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.hangu.tool.mybatis.secret.annotated.EnOrDecrypt;
 import com.hangu.tool.secret.dao.entity.SysUserInfo;
+import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 /**
@@ -16,4 +17,14 @@ import org.apache.ibatis.annotations.Param;
 public interface SysUserInfoMapper extends BaseMapper<SysUserInfo> {
 
     int updatePasswordByAccount(@EnOrDecrypt @Param("password") String password, @Param("account") String account);
+
+    List<SysUserInfo> queryByPassword(SysUserInfo query);
+
+    int deleteByPassword(@EnOrDecrypt @Param("password") String password);
+
+    int updateByPassword(@EnOrDecrypt @Param("newPassword") String newPassword,
+        @EnOrDecrypt @Param("oldPassword") String oldPassword);
+
+    int updateEntityByPassword(@Param("update") SysUserInfo update,
+        @EnOrDecrypt @Param("oldPassword") String oldPassword);
 }
